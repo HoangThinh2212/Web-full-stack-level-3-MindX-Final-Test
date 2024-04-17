7e9a4bcf1237922e8079bb0a81bae8613318e199
Hoang Thinh <hoangducthinh22@gmail.com>
2024-04-18 00:09:40 +0700

    Second Push

--- routes/movie.route.js
+++ routes/movie.route.js
@@ -1,18 +1,22 @@
 import express from "express";
 import {
   getMovies,
+  getMovieById,
   createMovie,
   updateMovie,
   deleteMovie,
+  searchMovies,
+  uploadImage,
 } from "../controllers/movie.controller.js";
 const router = express.Router();
 
+// Routes for movie CRUD operations
 router.get("/", getMovies);
-
+router.get("/:movieId", getMovieById);
 router.post("/", createMovie);
-
-router.put("/", updateMovie);
-
-router.delete("/", deleteMovie);
+router.put("/:movieId", updateMovie);
+router.delete("/:movieId", deleteMovie);
+router.get("/search", searchMovies);
+router.post("/movies/:movieId/upload-update-image", uploadImage);
 
 export default router;
